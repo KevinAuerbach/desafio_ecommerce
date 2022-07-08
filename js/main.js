6585024a766755f248e107e79917bc368d0bc79e
Kevin Auerbach <auerbachkevin123@gmail.com>
2022-07-08 18:07:36 -0300

    se agrego concepto de clase y array

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
-//Declaracion de variables
-let remera = 2500
-let jean = 8000
-let campera = 10600
-let zapatillas = 15000
-let medias = 1500
 
-//funcion que voy a utilizar
+
+//funciones que voy a utilizar
 function precioFinal(producto){
      return producto + (producto*21/100)
 }
+
+//creo la clase de los productos que vendo
+class producto {
+    constructor(prenda,color,precio){
+        this.prenda = prenda
+        this.color = color
+        this.precio = precio
+    }
+}
+let remera = new producto("remera","blanco",2500)
+let jean = new producto("jean","azul",8000)
+let campera = new producto("campera","negro",10600)
+let zapatillas = new producto("zapatillas","rojo",15000)
+let medias = new producto("medias","negro",1500)
+
+
+//array de productos
+let productos = [
+    {prenda:"remera", color:"blanco", precio:2500},
+    {prenda:"jean", color:"azul", precio:8000},
+    {prenda:"campera", color:"negro", precio: 10600},
+    {prenda:"zapatillas", color:"rojo", precio:15000},
+    {prenda:"medias", color:"negro", precio:1500}
+]
+
+
 
 
 //Comienzo del simulador
@@ -19,19 +40,19 @@ let carrito = prompt("Elija que prenda quiere llevar: (remera, jean, campera, za
 while(carrito != "salir"){
     switch(carrito){
         case "remera":
-            alert(`El precio de su producto es $${precioFinal(remera)}`)
+            alert(`El precio de su producto es $${precioFinal(remera.precio)}`)
             break
         case "jean":
-            alert(`El precio de su producto es $${precioFinal(jean)}`)
+            alert(`El precio de su producto es $${precioFinal(jean.precio)}`)
             break
         case "campera":
-            alert(`El precio de su producto es $${precioFinal(campera)}`)
+            alert(`El precio de su producto es $${precioFinal(campera.precio)}`)
             break
         case "zapatillas":
-            alert(`El precio de su producto es $${precioFinal(zapatillas)}`)
+            alert(`El precio de su producto es $${precioFinal(zapatillas.precio)}`)
             break
         case "medias":
-            alert(`El precio de su producto es $${precioFinal(medias)}`)
+            alert(`El precio de su producto es $${precioFinal(medias.precio)}`)
             break
         default:
             alert("Lo siento, ese producto no se encuentra en la tienda")
@@ -39,6 +60,10 @@ while(carrito != "salir"){
         }
     carrito = prompt("Elija otro producto que desee (remera, jean, campera, zapatillas o medias) o escriba salir")
 }
+
+
 alert("Gracias por visitar nuestra tienda, ojala vuelva pronto")
+
+
 
 
